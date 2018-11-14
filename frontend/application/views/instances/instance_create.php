00a4e8a621b445b7b01e56d72de42e544cd4ce7d
Felipe Diaz Bahamondes <felipe.diaz.bahamondes@gmail.com>
2018-11-14 13:26:16 -0300

    actualización de vista con estilos definidos

--- frontend/application/views/instances/instance_create.php
+++ frontend/application/views/instances/instance_create.php
@@ -1,14 +1,46 @@
-<h1>Creación de instancia</h1>
+<div class="col-12 grid-margin">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Crear instancia</h4>
+            <?php echo form_open(base_url() . 'instances/create', array('class', 'email')); ?>
+            <p class="card-description">
+                Crea una instancia a partir de la configuración de un modelo.
+            </p>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group row">
+                        <?php echo form_label('Nombre', 'name', array('class' => 'col-sm-3 col-form-label')) ?>
+                        <div class="col-sm-9">
+                            <?php echo form_input(array('name' => 'name', 'class' => 'form-control')) ?>
+                        </div>
+                        <?php echo form_error('name'); ?>
+
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label">Tipo</label>
+
+                        <div class="col-sm-9">
+                            <?php echo form_dropdown('shirts', $list_types, 'large', array('class'=>'form-control')); ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
-<?php echo form_open(base_url().'instances/create', 'class="email" id="myform"');?>
+            <div class="row">
+                <?php echo form_submit('submit', 'Crear instancia', array('class'=>'btn btn-success mr-2')) ?>
+                <button class="btn btn-light">Cancelar</button>
+            </div>
+            <?php echo form_close(); ?>
+        </div>
 
-<?php echo form_label('nombre','name') ?>
-<?php echo form_input('name') ?>
-<?php echo form_error('name'); ?>
+    </div>
+</div>
 
-<?php echo form_dropdown('shirts', $list_types, 'large'); ?>
 
-<?php echo form_submit('submit','crear instancia') ?>
 
-<?php echo form_close();?>
+
+
