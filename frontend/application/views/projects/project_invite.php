4998865fb7d2b880c4822729657490845883d59d
Omar <omarfernan94@gmail.com>
2018-11-20 10:22:44 -0300

    Arreglos varios, se crean plantilla para compartir.

---
+++ frontend/application/views/projects/project_invite.php
@@ -0,0 +1,10 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Compartir proyecto</h4>
+        <div class="card-description">
+            <select class="js-data-example-ajax" multiple="multiple"></select>
+        </div>
+    </div>
+</div>
