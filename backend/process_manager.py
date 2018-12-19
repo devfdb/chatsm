a3cbc8ed2f43bdf8ac6fbf27881d6de2a70c0682
Alvaro-0000 <gaaragak@gmail.com>
2018-12-19 17:40:56 -0300

    Actualización de las rutas de entradas y salidas

--- backend/process_manager.py
+++ backend/process_manager.py
@@ -6,9 +6,8 @@ from calendar import timegm
 from time import gmtime
 import uuid
 import time
-connection = None
 
-uid = ''
+connection = None
 
 try:
     # Establece la conexión con el servidor RabbitMQ.
@@ -24,211 +23,187 @@ channel.queue_declare(queue='tasks')
 def process(proc, epoch, project, _input, first):
     """
     Función iterativa que invoca todos los procesos solicitados desde el json de entrada.
-    :param proc: json con los procesos
+    :param proc: json con un arbol o sub_arbol de tareas. Solo toma el superior en una iteración.
     :param epoch: String con la dirección unica para el nuevo directorio de salida
     :param project: String con el nombre del proyecto.
     :param _input: String con la ruta relativa del archivo
     :param first: boolean que confirma la primera ejecución.
     :return: json basado en proc, con los nombres de los archivos generados.
     """
+
+    base_input_route = ''     # Nominal. Primera ruta esta vacia.
+    repository_route = os.path.join('..', 'repository')
+    # base_extra_files_route = os.path.join(repository_route, project, 'input')
+    # Placeholder. Ruta a usar con parametros
+    base_output_route = os.path.join(
+        repository_route, project, 'output', str(epoch)
+    )
     if proc:
         # Manejo de si es padre, determinando la carpeta de obtencion de archivo.
         if first:
-            rout = ''
+            actual_input_route = base_input_route
         else:
-            rout = os.path.join('output', str(epoch))
-        # Creacion del directorio objetivo
+            actual_input_route = os.path.join('output', str(epoch))
+
+        # Creacion del directorio de la ejecución actual.
         # Si no existe la carpeta 'epoch'
-        if not os.path.isdir(os.path.join('..', 'repository', project, 'output', str(epoch))):
-            # Si no existe la carpeta 'output', en caso de primera ejecución
-            if not os.path.isdir(os.path.join('..', 'repository', project, 'output')):
-                os.mkdir(os.path.join('..', 'repository', project, 'output'))
-            os.mkdir(os.path.join('..', 'repository', project, 'output', str(epoch)))
+
+        if not os.path.isdir(base_output_route):
+            # # Se consulta por la carpeta 'output'
+            # # Si no existe, en caso de primera ejecución
+            # if not os.path.isdir(os.path.join('..', 'repository', project, 'output')):
+            #     os.mkdir(os.path.join('..', 'repository', project, 'output'))
+            # Lo anterior es deprecado. Al estar ligada la carpeta output a la base de datos, es creada al crear el
+            # proyecto.
+            os.mkdir(base_output_route)
 
         # Definicion de la ruta de obtencion del archivo.
-        rout = os.path.join('..', 'repository', project, rout, _input)
+        actual_input_file_location = os.path.join(actual_input_route, _input)
 
-        # Definicion del nombre y la ruta de guardado del archivo.
-        output_route = os.path.join('..', 'repository', project, 'output', str(epoch))
-        output_name = 'clean_' + str(proc['id']) + '.csv'
-
-        # Dado que proc no se None, obtiene la tarea de este.
+        # Dado que proc no se None, obtiene la tarea de este, el nombre del archivo de salida y su ruta completa.
         task = proc['task']
+        output_filename = "Tarea_" + str(proc['id']) + ' - ' + task['name']
+        output_file_location = os.path.join(base_output_route, output_filename)
+
+        # Del mismo modo, como se sabe el nombre del archivo se pueden fijar parametros para la siguiente iteracion
+        task['output'] = output_filename
+        children_input = output_filename
+
         # Llama la tarea correspondiente a la tarea especificada.
         if task['name'] == 'cleaner':
             task['inicio'] = time.time()
             print("Time:", task['inicio'])
             print('Cleaning...')
-            c = service.Cleaner(rout, os.path.join(output_route, output_name), task['params'])
+            c = service.Cleaner(actual_input_file_location, output_file_location, task['params'])
             del c
             task['termino'] = time.time()
-            # Adjunta el nombre del archivo generado al json, en el campo 'output'
-            task['output'] = output_route
-            # Reemplaza la variable _input con el valor de output, en caso de que existiese una tarea hija
-            _input = output_name
-            print(_input)
         elif task['name'] == 'replace':
             task['inicio'] = time.time()
             print('Replacing...')
-            output_name = 'replace_' + str(proc['id']) + '.csv'
-            r = service.Replacer(rout, os.path.join(output_route, output_name), project, task['params'])
+            r = service.Replacer(actual_input_file_location, output_file_location, project, task['params'])
             del r
             task['termino'] = time.time()
             # Adjunta el nombre del archivo generado al json, en el campo 'output'
-            task['output'] = output_route
-            _input = output_name
-            print(_input)
         elif task['name'] == 'spellcheck':
             task['inicio'] = time.time()
             print('Spellchecking...')
-            output_name = 'spellcheck_' + str(proc['id']) + '.csv'
-            s = service.SpellChecker(rout, os.path.join(output_route, output_name), project, task['params'])
+            s = service.SpellChecker(actual_input_file_location, output_file_location, project, task['params'])
             del s
             task['termino'] = time.time()
             # Adjunta el nombre del archivo generado al json, en el campo 'output'
-            task['output'] = output_route
-            _input = output_name
-            print(_input)
-        if task['name'] == 'word2vec':
+        elif task['name'] == 'word2vec':
             task['inicio'] = time.time()
             print('Creating Word2Vec...')
-            output_name = 'word2vec_' + str(proc['id']) + '.csv'
-            s = service.Word2Vec(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.Word2Vec(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'svm_model':
+        elif task['name'] == 'svm_model':
             task['inicio'] = time.time()
             print('Creating SVM model...')
-            output_name = 'svm_' + str(proc['id']) + '.csv'
-            s = service.SVM(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.SVM(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'mnb_model':
+        elif task['name'] == 'mnb_model':
             task['inicio'] = time.time()
             print('Creating Multinomial NB model...')
-            output_name = 'mnb_' + str(proc['id']) + '.csv'
-            s = service.MultinomialNB(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.MultinomialNB(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'mlp_model':
+        elif task['name'] == 'mlp_model':
             task['inicio'] = time.time()
             print('Creating MLP Network model...')
-            output_name = 'mlp_' + str(proc['id']) + '.csv'
-            s = service.MLPClassifier(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.MLPClassifier(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'cluster':
+        elif task['name'] == 'cluster':
             task['inicio'] = time.time()
             print('Clustering...')
-            output_name = 'hcluster_' + str(proc['id']) + '.csv'
-            s = service.HierarchicClustering(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.HierarchicClustering(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'extract_cluster':
+        elif task['name'] == 'extract_cluster':
             task['inicio'] = time.time()
             print('Extracting clusters...')
-            output_name = 'clusterextract_' + str(proc['id']) + '.csv'
-            s = service.ClusterExtractor(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.ClusterExtractor(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'ner_trainer':
+        elif task['name'] == 'ner_trainer':
             task['inicio'] = time.time()
             print('Training NER...')
-            output_name = 'ner_training_' + str(proc['id']) + '.plk'
-            s = service.NERTrainer(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.NERTrainer(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'ner_training_generator':
+        elif task['name'] == 'ner_training_generator':
             task['inicio'] = time.time()
             print('Generating Training...')
-            output_name = 'ner_training_dataset_' + str(proc['id']) + '.csv'
-            s = service.NERTrainingGenerator(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.NERTrainingGenerator(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
-        if task['name'] == 'pos_tagger_trainer':
+        elif task['name'] == 'pos_tagger_trainer':
             task['inicio'] = time.time()
             print('Training POS-Tagger ...')
-            output_name = 'pos_training_' + str(proc['id']) + '.plk'
-            s = service.POStrainer(rout, os.path.join(output_route, output_name), task['params'])
+            s = service.POStrainer(actual_input_file_location, output_file_location, task['params'])
             del s
             task['termino'] = time.time()
-            task['output'] = output_name
-            _input = output_name
-            print(_input)
+
+        print("Generated file:", children_input)
+
+        # Finalmente, si existen hijos, se llama a si misma, con el nuevo _input como _input y con el subjson del hijo
+        # como tarea a realizar
         if 'children' in proc:
             children_list = []
-            # Si existen hijos, se llama a si misma, con el nuevo _input como _input y con el subjson del hijo como proc
+            # Recorrido como lista ya que hijos en un mismo nivel estan en una lista.
             for child in proc['children']:
-                children_list.append(process(child, epoch, project, _input, False))
+                children_list.append(process(child, epoch, project, children_input, False))
             proc['children'] = children_list
             print(proc)
+        # Finalmente, lo que retornado es la misma tarea, con información extra.
         return proc
 
 
-def send(message, uid):
+def send(message, unique_id):
     body = {
-        'id': uid,
+        'id': unique_id,
         'message': message
     }
+    reply_connection = None     # Asignación para consistencia del código.
 
     try:
         # Establece la conexión con el servidor RabbitMQ.
         reply_connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    except Exception as e:
-        print("Error de conexion", e)
+    except Exception as error:
+        print("Error de conexion", error)
         exit(2)
 
     reply_channel = reply_connection.channel()
 
     reply_channel.queue_declare(queue='reply')
 
-    reply_channel.basic_publish(exchange='',
-                          routing_key='reply',
-                          body=json.dumps(body))
+    reply_channel.basic_publish(
+        exchange='',
+        routing_key='reply',
+        body=json.dumps(body)
+    )
     reply_connection.close()
 
 
 def callback(ch, method, props, bodys):
     """
     callback se encarga de manejar las entradas y salidas entre el servidor y el cliente.
-    :param ch:
+    :param ch:  canal de conexión rabbitMQ
     :param method:
     :param props:
-    :param body: json recibido del cliente.
+    :param bodys: json recibido del cliente.
     :return: nada
     """
     if bodys:
 
-        uid = str(uuid.uuid4())
+        unique_id = str(uuid.uuid4())
 
         response = {
             'result': 'processing',
             'data': {
-                'id_execution': uid
+                'id_execution': unique_id
             }
         }
 
@@ -249,7 +224,7 @@ def callback(ch, method, props, bodys):
                 result_list.append(process(proc, timegm(gmtime()), project, _input, True))
             print('Listo')
             body['processes'] = result_list
-            send(body, uid)
+            send(body, unique_id)
 
         except json.decoder.JSONDecodeError as err:
             print('ERRRRRRRRROOOOOOOOOOORRRRRRRRRR')
@@ -266,7 +241,6 @@ def callback(ch, method, props, bodys):
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
     else:
-
         response = {
             'result': 'error',
             'message': 'Json esta vacio'
